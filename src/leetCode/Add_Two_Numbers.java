749ba08d93601bd83ae506846c8f392c571e6230
easyworld <easy_world_118@hotmail.com>
2015-07-07 20:34:45 +0800

    add two numbers

--- src/leetCode/Add_Two_Numbers.java
+++ src/leetCode/Add_Two_Numbers.java
@@ -1,16 +1,36 @@
 package leetCode;
 
 public class Add_Two_Numbers {
-	public class ListNode {
-		int val;
-		ListNode next;
 
-		ListNode(int x) {
-			val = x;
+	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		int carry = 0;
+		ListNode head = new ListNode(0);
+		ListNode tail = head;
+		while (l1 != null || l2 != null || carry != 0) {
+			carry += (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+			tail.next = new ListNode(carry % 10);
+			carry /= 10;
+			tail = tail.next;
+			l1 = (l1 == null ? null : l1.next);
+			l2 = (l2 == null ? null : l2.next);
 		}
+		return head.next;
 	}
 
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		return null;
+	public static void test(String s) {
+		s = "FUCK";
+	}
+
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(2);
+		// l1.next = new ListNode(4);
+		// l1.next.next = new ListNode(3);
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
+		ListNode re = addTwoNumbers(l1, l2);
+		for (ListNode l = re; l != null; l = l.next) {
+			System.out.println(l.val);
+		}
 	}
 }
